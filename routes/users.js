6898aa482f7017c418f476d80e5b5c1645d5e8cc
Mikhail Fatkin <mfo787@gmail.com>
2020-01-26 12:45:29 +0300

    correct routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ const userRoute = require('express').Router();
 
 const { getUsers } = require('../controllers/users');
 
-userRoute.get('/users/me', getUsers);
+const auth = require('../middlewares/auth');
+
+const validation = require('../helpers/validation');
+
+userRoute.get('/users/me', validation.signin, auth, getUsers);
 
 
 module.exports = userRoute;
